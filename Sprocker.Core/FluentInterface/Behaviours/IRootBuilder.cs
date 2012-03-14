d9effb1743c0b4dfbc1a64372b9b06206dae0208
Stonie <andrew@stonie.net>
2012-03-15 07:18:15 +1100

    Wip FluentInterface

---
+++ Sprocker.Core/FluentInterface/Behaviours/IRootBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+
+namespace TheSprocker.Core.FluentInterface.Behaviours
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T">this is required for the row mapper</typeparam>
+    public interface IRootMapBuilder
+    {
+        IRootMapBuilder Proc(String procedureName);
+        IRootMapBuilder AutoMapAll(); // another interface for the exclusions
+        void Build();
+    }
+}
