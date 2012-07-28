62b49dba6973f8011a10813f06fd3c9b8bea6103
neutmute <wintermute111@gmail.com>
2012-07-28 13:43:20 +1000

    cleaning up

---
+++ README.md
@@ -0,0 +1,1 @@
+Some helping wrappers around Entlib5 
--- Sprocker.Core/SprockerCommand.cs
+++ Sprocker.Core/SprockerCommand.cs
@@ -205,17 +205,6 @@ namespace TheSprocker.Core
             }
         }
 
-        //public IEnumerable<TResult> MapSet(IEnumerable<DataRow> dataRows)
-        //{
-        //    DataTable dataTable = dataRows.CopyToDataTable();
-        //    //return MapSet(dataTable)
-        //    //foreach (DataRow dataRow in dataRows)
-        //    //{
-        //    //    var iDataRecord = new DataRowAdapter(dataRow);
-        //    //    yield return _rowMapper.MapRow(iDataRecord);
-        //    //}
-        //    return null;
-        //}
     }
     #endregion
 
