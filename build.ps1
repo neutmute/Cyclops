3131b7d791101cadd1c28f0855945a83af7b8d57
neutmute <neutmute@users.noreply.github.com>
2015-11-26 11:43:51 +1100

    gitlink

--- build.ps1
+++ build.ps1
@@ -3,38 +3,79 @@ param(
     [string]$configuration = "Release"
 )
 
+. ".\build.common.ps1"
+
+function restorePackages{
+    _WriteOut -ForegroundColor $ColorScheme.Banner "nuget, gitlink restore"
+    _DownloadNuget $packagesFolder
+    nuget restore
+    nuget install gitlink -SolutionDir "$rootFolder" -ExcludeVersion
+}
+
+function nugetPack{
+    _WriteOut -ForegroundColor $ColorScheme.Banner "Nuget pack"
+    
+    New-Item -Force -ItemType directory -Path $rootFolder\_output
+
+    if(!(Test-Path Env:\nuget )){
+        $env:nuget = nuget
+    }
+    if(!(Test-Path Env:\PackageVersion )){
+        $env:PackageVersion = "1.0.0.0"
+    }
+
+    #&$env:nuget pack $rootFolder\Source\Cyclops\Cyclops.csproj -o _output -p Configuration=$configuration -Version $env:PackageVersion
+    #&$env:nuget pack $rootFolder\Source\Cyclops.DependencyInjection\Cyclops.DependencyInjection.csproj -o _output -p Configuration=$configuration -Version $env:PackageVersion
+    nuget pack $rootFolder\Source\Cyclops\Cyclops.csproj -o _output -p Configuration=$configuration -Version $env:PackageVersion
+    nuget pack $rootFolder\Source\Cyclops.DependencyInjection\Cyclops.DependencyInjection.csproj -o _output -p Configuration=$configuration -Version $env:PackageVersion
+}
+
+function nugetPublish{
+
+    if(Test-Path Env:\BuildRunner ){
+        _WriteOut -ForegroundColor $ColorScheme.Banner "Nuget publish"
+        &$env:nuget push .\_output\* $env:nugetapikey
+    }
+
+}
+
+function buildSolution{
+
+    _WriteOut -ForegroundColor $ColorScheme.Banner "Build Solution"
+    & $msbuild "$rootFolder\$solutionName.sln" /p:Configuration=$configuration
+
+    &"$rootFolder\packages\gitlink\lib\net45\GitLink.exe" $rootFolder -u $sourceUrl
+}
+
+function executeTests{
+
+    Write-Host "Execute Tests"
+    if(!(Test-Path Env:\VsTestConsole )){
+        $env:VsTestConsole = "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+    }
+    & $env:VsTestConsole .\Source\Cyclops.Tests\bin\$configuration\Cyclops.Tests.dll /TestCaseFilter:"TestCategory!=SqlIntegration"
+
+}
+
 # Initialization
 $rootFolder = Split-Path -parent $script:MyInvocation.MyCommand.Path
 $rootFolder = Join-Path $rootFolder .
+$packagesFolder  = Join-Path $rootFolder packages
 $msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
-
-New-Item -Force -ItemType directory -Path $rootFolder\_output
 
 # Solution
 $solutionName = "cyclops"
+$sourceUrl = "https://github.com/neutmute/Cyclops"
 
-Write-Host "Build Solution"
-& $msbuild "$rootFolder\$solutionName.sln" /p:Configuration=$configuration
+_WriteOut -ForegroundColor $ColorScheme.Banner "-= Cyclops Build =-"
+_WriteConfig "rootFolder" $rootFolder
 
-Write-Host "Execute Tests"
-if(!(Test-Path Env:\VsTestConsole )){
-    $env:VsTestConsole = "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
-}
-& $env:VsTestConsole .\Source\Cyclops.Tests\bin\$configuration\Cyclops.Tests.dll /TestCaseFilter:"TestCategory!=SqlIntegration"
+restorePackages
 
-Write-Host "Nuget pack"
-if(!(Test-Path Env:\nuget )){
-    $env:nuget = "$rootFolder\.nuget\nuget.exe"
-}
-if(!(Test-Path Env:\version )){
-    $env:version = "1.0.0.0"
-}
+buildSolution
 
-&$env:nuget pack $rootFolder\Source\Cyclops\Cyclops.csproj -o _output -p Configuration=$configuration -Version $env:PackageVersion
-&$env:nuget pack $rootFolder\Source\Cyclops.DependencyInjection\Cyclops.DependencyInjection.csproj -o _output -p Configuration=$configuration -Version $env:PackageVersion
+executeTests
 
-if(Test-Path Env:\BuildRunner ){
-    Write-Host "Nuget publish"
-    &$env:nuget push .\_output\* $env:nugetapikey
-}
+nugetPack
 
+nugetPublish
